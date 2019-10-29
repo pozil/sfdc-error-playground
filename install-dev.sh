6efa476d82e1728a05abd9d83ef9b88a57d8b4bb
pozil <philippe.ozil@gmail.com>
2019-10-29 10:44:28 +0100

    Added installation script

---
+++ install-dev.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Set parameters
+ORG_ALIAS="playground"
+
+echo ""
+echo "Installing Playground org:"
+echo "- Org alias:      $ORG_ALIAS"
+echo ""
+
+# Install script
+echo "Cleaning previous scratch org..."
+sfdx force:org:delete -p -u $ORG_ALIAS &> /dev/null
+echo ""
+
+echo "Creating scratch org..." && \
+sfdx force:org:create -s -f config/project-scratch-def.json -a $ORG_ALIAS -d 30 && \
+echo "" && \
+
+echo "Pushing source..." && \
+sfdx force:source:push -f -u $ORG_ALIAS && \
+echo ""
+
+EXIT_CODE="$?"
+
+# Check exit code
+echo ""
+if [ "$EXIT_CODE" -eq 0 ]; then
+  echo "Installation completed."
+  echo ""
+  sfdx force:org:open -p /c/ErrorPlaygroundApp.app -u $ORG_ALIAS
+else
+    echo "Installation failed."
+fi
+
+exit $EXIT_CODE
